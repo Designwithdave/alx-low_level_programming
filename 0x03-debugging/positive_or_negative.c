4ef0effc24043017a30a94571d05160c54ef88f1
Moluno Elivs <emoluno@gmail.com>
2023-03-24 05:02:19 +0000

    0x03-debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* positve_or_negative - checks for positve or negative numbers
+* positive_or_negative - checks for positive or negative numbers
 *
-*@i:the number to the checked
+*@i: the number to be checked
 *
 *Return: always 0
 */
 void positive_or_negative(int i)
 {
 if (i < 0)
-printf("%d id positive\n", i);
+printf("%d is negative\n", i);
 else
 printf("%d is positive\n", i);
 }
