283d7da795519f7f5848e0b665c154b974c22330
Moluno Elivs <emoluno@gmail.com>
2023-03-24 04:53:54 +0000

    0x03-debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* positve_or_negative - checks for positve or negative numbers
+*
+*@i:the number to the checked
+*
+*Return: always 0
+*/
+void positive_or_negative(int i)
+{
+if (i < 0)
+printf("%d id positive\n", i);
+else
+printf("%d is positive\n", i);
+}
