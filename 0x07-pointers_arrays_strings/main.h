c3c86d7de76eb918a10b244a68a347613e319127
Moluno Elivs <emoluno@gmail.com>
2023-04-04 22:21:35 +0000

    4-strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,10 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 
+unsigned int _strspn(char *s, char *accept);
+
+size_t strspn(const char *str1, const char *str2);
+
+ char *_strpbrk(char *s, char *accept);
+
 #endif /* MAIN_H */
