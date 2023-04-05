1aaf6d23d3d3f3dbdd931d20e5b2afbfdf241d4d
Moluno Elivs <emoluno@gmail.com>
2023-04-05 14:28:12 +0000

    6-prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,7 +13,8 @@ if (n <= 3)
 return (1);
 if (n % 2 == 0 || n % 3 == 0)
 return (0);
-for (int i = 5; i * i <= n; i += 6) {
+for (int i = 5; i * i <= n; i += 6)
+{
 if (n % i == 0 || n % (i + 2) == 0)
 return (0);
 }
